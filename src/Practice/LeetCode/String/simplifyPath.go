2483941b9012b9e191b57650422330f32612d9d6
Sanjeev Singh <sanjeev@Sanjeevs-MacBook-Air.local>
2024-04-17 19:47:45 -0700

    upload Simplify Path

---
+++ src/Practice/LeetCode/String/simplifyPath.go
@@ -0,0 +1,44 @@
+// 71. Simplify Path
+// https://leetcode.com/problems/simplify-path/
+
+type Stack []string
+func (s *Stack) IsEmpty() bool {
+    return len(*s) == 0
+}
+func (s *Stack) Push(str string) {
+    (*s) = append((*s), str)
+}
+
+func (s *Stack) Pop() {
+    index := len(*s) -1
+    (*s) = (*s)[:index]
+}
+
+func simplifyPath(path string) string {
+    
+    s := &Stack{}
+    
+    var strs []string
+    strs = strings.Split(path, "/")
+
+    for _, str := range strs {
+        if str == "" || str == "." {
+            continue
+        } else if str == ".." {
+            if !s.IsEmpty() {
+                s.Pop()
+            }
+        } else {
+            s.Push(str)
+        }
+    }
+
+    build := ""
+    if len(*s) == 0 { return "/"} // Important
+    for pos := 0; pos < len(*s); pos++ {
+        build += "/"
+        build +=  (*s)[pos]
+    }
+    
+    return build
+}
